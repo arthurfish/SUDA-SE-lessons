3e7ca7b8242fe0a01efbbebf1fe68447c5a95ac2
arthur <arthurfish8051@gmail.com>
2025-04-03 12:20:04 +0800

    remove big file

--- COMS2044 编译原理/新实验课/实验10/example.py
+++
@@ -1,6 +0,0 @@
-a=1
-b=2
-c=a+b
-d=c-1+a
-print(c)
-print(a,b,c)
--- COMS2044 编译原理/新实验课/实验2/FPDF教程/例子/example1.py
+++
@@ -1,9 +0,0 @@
-#! /usr/bin/env python
-#coding=utf-8
-from fpdf import FPDF
-
-pdf = FPDF()
-pdf.add_page()
-pdf.set_font('Arial', 'B', 16)
-pdf.cell(40, 10, 'Hello World!')
-pdf.output('tuto1.pdf', 'F')
